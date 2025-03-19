654b4ab6b638720540c09d0dadec63955da5819b
Leomsp7 <leosignoreto@gmail.com>
2025-03-19 17:51:12 -0300

    Início do trabalho aula 03

---
+++ aula03/Aula03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="POST" action="Aula03.php">
+        Nome: <input type="text" name="nome" required><br>
+        RA: <input type="text" name="ra" required><br>
+        Curso: <input type="text" name="curso" required><br>
+        Endereço: <input type="text" name="endereço" required><br>
+          <button type="submit" name="submit" value="adicionar">Adicionar</button>
+      </form>
+
+   
+</body>
+</html>
---
+++ aula03/MYSQL.sql
@@ -0,0 +1,11 @@
+create database if not exists Aula03;
+USE Aula03;
+
+create table Registro(
+
+Nome varchar(40),
+RA int(20) primary key,
+Curso varchar(30),
+Endereço varchar(50)
+);
+
